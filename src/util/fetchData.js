2513828e2821395b6797c43e0ea952ddec208270
Mahmoud Hamdy <76693552+Mahmoud81-dev@users.noreply.github.com>
2022-07-30 22:48:48 +0200

    Update fetchData.js

--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -1,9 +1,9 @@
 export const exerciseOptions = {
-  method: "GET",
+method: "GET",
   headers: {
-    "X-RapidAPI-Key": "10260816e4msh4f28294e8d5b291p1e0532jsn1646f1244c67",
+    "X-RapidAPI-Key": "3ef79bd6e4msh2182cc4c74cad3dp156f25jsn5b546fc569b6",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-  },
+  }
 };
 
 export const youtubeOptions = {
